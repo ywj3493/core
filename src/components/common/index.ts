ce675b6410fc8a5679184bae845d999243ce0694
ywj3493 <ywj3493@naver.com>
2024-04-01 13:45:13 +0900

    fix: 구조 변경

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,1 @@
-export * from "./InfiniteScrollSection";
-export * from "./InvisibleIntersectionTarget";
+export * from "./intersection";
---
+++ src/components/common/intersection/index.ts
@@ -0,0 +1,2 @@
+export * from "./InvisibleIntersectionTarget";
+export * from "./InfiniteScrollSection";
